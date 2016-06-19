d2f3c171a849a5b420b58e4ee5f06fe6a6ef5d90
Shad3Light <shad.3ver.light@gmail.com>
2016-06-19 21:47:58 +0700

    Various Cards
    Eye of Illusion
    Brohunder
    Line Monster K Horse
    Mokusatsu
    Name Erasure
    Negative Energy
    Negative Energy Generator

---
+++ scripts/c511005009.lua
@@ -0,0 +1,67 @@
+--Mokusatsu
+--  By Shad3
+
+local self=c511005009
+
+function self.initial_effect(c)
+  --Activate
+  local e1=Effect.CreateEffect(c)
+  e1:SetType(EFFECT_TYPE_ACTIVATE)
+  e1:SetCode(EVENT_DRAW)
+  e1:SetCategory(CATEGORY_HANDES+CATEGORY_DRAW)
+  e1:SetCondition(self.cont_cd)
+  e1:SetTarget(self.cont_tg)
+  e1:SetOperation(self.cont_op)
+  c:RegisterEffect(e1)
+  --Continuous
+  local e2=Effect.CreateEffect(c)
+  e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+  e2:SetCode(EVENT_DRAW)
+  e2:SetRange(LOCATION_SZONE)
+  e2:SetCategory(CATEGORY_HANDES+CATEGORY_DRAW)
+  e2:SetCondition(self.cont_cd)
+  e2:SetTarget(self.cont_tg)
+  e2:SetOperation(self.cont_op)
+  c:RegisterEffect(e2)
+end
+
+function self.cont_cd(e,tp,eg,ep,ev,re,r,rp)
+  return ep~=tp and r==REASON_RULE
+end
+
+function self.cont_fil1(c)
+  return c:IsLocation(LOCATION_HAND)
+end
+
+function self.cont_fil2(c,i)
+  return c:IsCode(i)
+end
+
+function self.cont_tg(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return true end
+  e:SetLabel(Duel.AnnounceCard(tp))
+  Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,1,1-tp,0)
+  Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,1,1-tp,0)
+end
+
+function self.cont_op(e,tp,eg,ep,ev,re,r,rp)
+  local tg=eg:Filter(self.cont_fil1,nil)
+  if tg:GetCount()>0 then
+    local i=e:GetLabel()
+    Duel.ConfirmCards(tp,tg)
+    if tg:IsExists(self.cont_fil2,1,nil,i) then
+      Duel.SendtoGrave(tg:Filter(self.cont_fil2,nil,i),REASON_EFFECT+REASON_DISCARD)
+      Duel.ShuffleHand(1-tp)
+      local e1=Effect.CreateEffect(e:GetHandler())
+      e1:SetType(EFFECT_TYPE_FIELD)
+      e1:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+      e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+      e1:SetReset(RESET_PHASE+PHASE_END)
+      e1:SetTargetRange(0,1)
+      Duel.RegisterEffect(e1,tp)
+    else
+      Duel.ShuffleHand(1-tp)
+      if Duel.SelectYesNo(1-tp,aux.Stringid(511005009,0)) then Duel.Draw(1-tp,1,REASON_EFFECT) end
+    end
+  end
+end
---
+++ scripts/c511005010.lua
@@ -0,0 +1,45 @@
+--Name Erasure
+--  By Shad3
+
+local self=c511005010
+
+function self.initial_effect(c)
+  --Activate
+  local e1=Effect.CreateEffect(c)
+  e1:SetType(EFFECT_TYPE_ACTIVATE)
+  e1:SetCode(EVENT_FREE_CHAIN)
+  c:RegisterEffect(e1)
+  --Effect
+  local e2=Effect.CreateEffect(c)
+  e2:SetType(EFFECT_TYPE_IGNITION)
+  e2:SetRange(LOCATION_SZONE)
+  e2:SetCategory(CATEGORY_HANDES+CATEGORY_DAMAGE)
+  e2:SetTarget(self.tg)
+  e2:SetOperation(self.op)
+  e2:SetCountLimit(1)
+  c:RegisterEffect(e2)
+end
+
+function self.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+  if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,0,LOCATION_HAND,1,nil) end
+  e:SetLabel(Duel.AnnounceCard(tp))
+  Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,1,1-tp,0)
+  Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,1000,tp,0)
+end
+
+function self.code_fil(c,i)
+  return c:IsCode(i)
+end
+
+function self.op(e,tp,eg,ep,ev,re,r,rp)
+  local g=Duel.GetMatchingGroup(Card.IsDiscardable,tp,0,LOCATION_HAND,nil)
+  local i=e:GetLabel()
+  Duel.ConfirmCards(tp,g)
+  local tg=g:Filter(self.code_fil,nil,i)
+  if tg:GetCount()>0 then
+    Duel.SendtoGrave(tg,REASON_EFFECT+REASON_DISCARD)
+  else
+    Duel.Damage(tp,1000,REASON_EFFECT)
+  end
+  Duel.ShuffleHand(1-tp)
+end
