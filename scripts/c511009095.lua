971ce8a448ca99c91d01d68ad82f74c8f6b1eddc
ClementLouis <clementlouis.gallier@viacesi.fr>
2016-07-18 11:15:06 +0200

    updated CWFD

--- scripts/c511009095.lua
+++ scripts/c511009095.lua
@@ -1,5 +1,5 @@
 --Clear Wing Fast Dragon
-function c511009095.initial_effect(c)
+function c95100843.initial_effect(c)
 	--synchro summon
 	aux.AddSynchroProcedure(c,nil,aux.NonTuner(nil),1)
 	c:EnableReviveLimit()
@@ -7,25 +7,26 @@ function c511009095.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(15240238,0))
 	e1:SetCategory(CATEGORY_DISABLE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetTarget(c511009095.target)
-	e1:SetOperation(c511009095.operation)
+	e1:SetTarget(c95100843.target)
+	e1:SetOperation(c95100843.operation)
 	c:RegisterEffect(e1)
 end
-function c511009095.filter(c)
+function c95100843.filter(c)
 	return c:IsFaceup() and c:GetAttack()>0
 end
-function c511009095.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and c511009095.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c511009095.filter,tp,0,LOCATION_MZONE,1,nil) end
+function c95100843.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and c95100843.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c95100843.filter,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectTarget(tp,c511009095.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp,c95100843.filter,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
 end
-function c511009095.operation(e,tp,eg,ep,ev,re,r,rp)
+function c95100843.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) and tc:GetAttack()>0 then
