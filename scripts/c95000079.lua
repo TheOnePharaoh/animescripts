a2db662f836275c0fa51d4e0ea701a345520fe16
ClementLouis <clementlouis.gallier@viacesi.fr>
2016-09-11 13:29:35 +0200

    action Field Fix (#86)
    * action Field Fix
    
    * action Field Fix

--- scripts/c95000079.lua
+++ scripts/c95000079.lua
@@ -6,63 +6,65 @@ function c95000079.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_PREDRAW)
 	e1:SetCountLimit(1)
-	e1:SetRange(0xff)
+	e1:SetRange(LOCATION_HAND+LOCATION_DECK)
 	e1:SetOperation(c95000079.op)
 	c:RegisterEffect(e1)
-	--unaffectable
+	--redirect
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-	e2:SetValue(1)
-	c:RegisterEffect(e2)
-	local e3=e2:Clone()
-	e3:SetCode(EFFECT_IMMUNE_EFFECT)
-	e3:SetValue(c95000079.ctcon2)
-	c:RegisterEffect(e3)
-	--cannot set
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_FIELD)
-	e4:SetCode(EFFECT_CANNOT_SSET)
-	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
-	e4:SetRange(LOCATION_SZONE)
-	e4:SetTargetRange(1,1)
-	e4:SetTarget(c95000079.aclimit2)
-	c:RegisterEffect(e4)
-	--~ Add Action Card
+	e2:SetCode(EVENT_LEAVE_FIELD)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetOperation(c95000079.repop)
+	c:RegisterEffect(e2)		
+	--unaffectable
 	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(95000079,0))
-	e5:SetType(EFFECT_TYPE_QUICK_O)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e5:SetRange(LOCATION_SZONE)
-	e5:SetCode(EVENT_FREE_CHAIN)
-	e5:SetCondition(c95000079.condition)
-	e5:SetTarget(c95000079.Acttarget)
-	e5:SetOperation(c95000079.operation)
-	c:RegisterEffect(e5)	
-	--Atk up
-	local e11=Effect.CreateEffect(c)
-	e11:SetType(EFFECT_TYPE_FIELD)
-	e11:SetRange(LOCATION_SZONE)
-	e11:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e11:SetCode(EFFECT_UPDATE_ATTACK)
-	e11:SetTarget(c95000079.filterStat)
-	e11:SetValue(250)
-	c:RegisterEffect(e11)
-	--Def down
-	local e12=Effect.CreateEffect(c)
-	e12:SetType(EFFECT_TYPE_FIELD)
-	e12:SetRange(LOCATION_SZONE)
-	e12:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e12:SetCode(EFFECT_UPDATE_DEFENSE)
-	e12:SetTarget(c95000079.filterStat)
-	e12:SetValue(-200)
-	c:RegisterEffect(e12)
+	e5:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e5:SetValue(1)
+	c:RegisterEffect(e5)
+	local e6=e5:Clone()
+	e6:SetCode(EFFECT_IMMUNE_EFFECT)
+	e6:SetValue(c95000079.ctcon2)
+	c:RegisterEffect(e6)
+	
+	-- Add Action Card
+	local e8=Effect.CreateEffect(c)
+	e8:SetDescription(aux.Stringid(95000079,0))
+	e8:SetType(EFFECT_TYPE_QUICK_O)
+	e8:SetRange(LOCATION_SZONE)
+	e8:SetCode(EVENT_FREE_CHAIN)
+	e8:SetCondition(c95000079.condition)
+	e8:SetTarget(c95000079.Acttarget)
+	e8:SetOperation(c95000079.operation)
+	c:RegisterEffect(e8)
+
+	--Atk
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_FIELD)
+	e6:SetCode(EFFECT_UPDATE_ATTACK)
+	e6:SetRange(LOCATION_SZONE)
+	e6:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e6:SetTarget(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_EARTH))
+	e6:SetCondition(c95000079.Fcond)
+	e6:SetValue(250)
+	c:RegisterEffect(e6)
+	--Def
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_FIELD)
+	e7:SetCode(EFFECT_UPDATE_DEFENCE)
+	e7:SetRange(LOCATION_SZONE)
+	e7:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e7:SetTarget(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_EARTH))
+	e7:SetCondition(c95000079.Fcond)
+	e7:SetValue(-200)
+	c:RegisterEffect(e7)
+	
 	--cannot change zone
 	local eb=Effect.CreateEffect(c)
 	eb:SetType(EFFECT_TYPE_SINGLE)
 	eb:SetCode(EFFECT_CANNOT_TO_DECK)
-	eb:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	eb:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	eb:SetRange(LOCATION_SZONE)
 	c:RegisterEffect(eb)
 	local ec=eb:Clone()
@@ -73,22 +75,33 @@ function c95000079.initial_effect(c)
 	c:RegisterEffect(ed)
 	local ee=eb:Clone()
 	ee:SetCode(EFFECT_CANNOT_REMOVE)
-	c:RegisterEffect(ee)		
-
+	c:RegisterEffect(ee)
 	
 end
 function c95000079.ctcon2(e,re)
 	return re:GetHandler()~=e:GetHandler()
 end
-function c95000079.aclimit2(e,c)
-	return c:IsType(TYPE_FIELD)
+--speed Duel Filter
+function c95000079.SDfilter(c)
+	return c:GetCode()==511004001
 end
-function c95000079.tgn(e,c)
-	return c==e:GetHandler()
+--vanilla mode filter
+function c95000079.Vfilter(c)
+	return c:GetCode()==511004002
 end
 function c95000079.op(e,tp,eg,ep,ev,re,r,rp,chk)
-local tc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
+	local tc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
 	local tc2=Duel.GetFieldCard(1-tp,LOCATION_SZONE,5)	
+	
+	--check if number of card >20 if speed duel or >40 if other duel
+	if Duel.IsExistingMatchingCard(c95000079.SDfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_REMOVED,0,1,nil) and Duel.GetMatchingGroup(nil,tp,LOCATION_HAND+LOCATION_DECK,0,nil):GetCount()<20 then
+	Duel.Win(1-tp,0x55)
+	end
+	if Duel.GetMatchingGroup(nil,tp,LOCATION_HAND+LOCATION_DECK,0,e:GetHandler()):GetCount()<40 and not Duel.IsExistingMatchingCard(c95000079.SDfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_REMOVED,0,1,nil) then
+	Duel.Win(1-tp,0x55)
+	end
+	
+	--move to field
 	if tc==nil then
 		Duel.MoveToField(e:GetHandler(),tp,tp,LOCATION_SZONE,POS_FACEUP,true)
 		if tc2==nil then
@@ -104,14 +117,29 @@ local tc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
 			Duel.MoveToField(token,tp,1-tp,LOCATION_SZONE,POS_FACEUP,true)
 			Duel.SpecialSummonComplete()
 		end
+		-- add ability Yell when Vanilla mode activated
+		-- if Duel.IsExistingMatchingCard(c95000079.Vfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_REMOVED,0,1,nil) then
+			-- c95000079.tableAction.push(95000200)
+		-- end
 	else
-		local WIN_REASON_ACTION_FIELD=0x55
-		Duel.Win(1-tp,WIN_REASON_ACTION_FIELD)
+		Duel.SendtoDeck(e:GetHandler(),nil,-2,REASON_EFFECT)
 	end
 	if e:GetHandler():GetPreviousLocation()==LOCATION_HAND then
 		Duel.Draw(tp,1,REASON_RULE)
 	end
 end
+
+function c95000079.aclimit(e,re)
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_SPELL) and re:GetHandler():IsType(TYPE_FIELD)
+end
+function c95000079.aclimit2(e,c)
+	return c:IsType(TYPE_FIELD)
+end
+function c95000079.tgn(e,c)
+	return c==e:GetHandler()
+end
+
+
 -- Add Action Card
 function c95000079.Acttarget(e,tp,eg,ep,ev,re,r,rp,chk)
 if chk==0 then return true end
@@ -122,82 +150,93 @@ local tc=g:GetFirst()
 math.randomseed( tc:getcode() )
 end
 i = math.random(20)
-ac=math.random(1,tableAction_size)
+ac=math.random(1,#tableAction)
 e:SetLabel(tableAction[ac])
 end
 function c95000079.operation(e,tp,eg,ep,ev,re,r,rp)
 if Duel.SelectYesNo(1-tp,aux.Stringid(95000079,0)) then
 local dc=Duel.TossDice(tp,1)
-if dc==2 or dc==3 or dc==4 or dc==6 then
-e:GetHandler():RegisterFlagEffect(95000079,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
+if dc==2 or dc==4 or dc==6 then
+
+Duel.RegisterFlagEffect(tp,95000079,RESET_PHASE+PHASE_END,0,1)
 end
 if dc==1 or dc==2 then
-if not Duel.IsExistingMatchingCard(c95000079.cfilter,tp,LOCATION_SZONE+LOCATION_HAND,0,1,nil) then
-		local token=Duel.CreateToken(tp,e:GetLabel(),nil,nil,nil,nil,nil,nil)		
-		Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetCode(EFFECT_CHANGE_TYPE)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetReset(RESET_EVENT+0x1fc0000)
-		e1:SetValue(TYPE_SPELL+TYPE_QUICKPLAY)
-		token:RegisterEffect(e1)
+if not Duel.IsExistingMatchingCard(c95000079.cfilter,tp,LOCATION_SZONE+LOCATION_HAND,0,1,nil) then	
+		  local token=Duel.CreateToken(tp,e:GetLabel())
 		Duel.SendtoHand(token,nil,REASON_EFFECT)
-		Duel.SpecialSummonComplete()	
 end
 end
 
 if dc==5 or dc==6 then
  if not Duel.IsExistingMatchingCard(c95000079.cfilter,1-tp,LOCATION_SZONE+LOCATION_HAND,0,1,nil) then
-		local token=Duel.CreateToken(1-tp,e:GetLabel(),nil,nil,nil,nil,nil,nil)		
-		Duel.SpecialSummonStep(token,0,1-tp,1-tp,false,false,POS_FACEUP)
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetCode(EFFECT_CHANGE_TYPE)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetReset(RESET_EVENT+0x1fc0000)
-		e1:SetValue(TYPE_SPELL+TYPE_QUICKPLAY)
-		token:RegisterEffect(e1)
-		Duel.SendtoHand(token,1-tp,REASON_EFFECT)
-		Duel.SpecialSummonComplete()
+		  local token=Duel.CreateToken(1-tp,e:GetLabel())
+		Duel.SendtoHand(token,nil,REASON_EFFECT)
 		end
 
 end
 
-else 
-if not Duel.IsExistingMatchingCard(c95000079.cfilter,tp,LOCATION_SZONE+LOCATION_HAND,0,1,nil) then
-		local token=Duel.CreateToken(tp,e:GetLabel(),nil,nil,nil,nil,nil,nil)		
-		Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetCode(EFFECT_CHANGE_TYPE)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetReset(RESET_EVENT+0x1fc0000)
-		e1:SetValue(TYPE_SPELL+TYPE_QUICKPLAY)
-		token:RegisterEffect(e1)
-		Duel.SendtoHand(token,nil,REASON_EFFECT)
-		Duel.SpecialSummonComplete()	
 end
-end
-end
-
-function c95000079.aclimit2(e,c)
-	return c:IsType(TYPE_FIELD)
 end
 function c95000079.condition(e,tp,eg,ep,ev,re,r,rp)
-	return not Duel.IsExistingMatchingCard(c95000079.cfilter,tp,LOCATION_SZONE+LOCATION_HAND,0,1,nil) and e:GetHandler():GetFlagEffect(95000079)==0
+	return not Duel.IsExistingMatchingCard(c95000079.cfilter,tp,LOCATION_SZONE+LOCATION_HAND,0,1,nil)
+	and Duel.GetFlagEffect(e:GetHandlerPlayer(),95000079)==0
 	and not e:GetHandler():IsStatus(STATUS_CHAINING)
 end
 function c95000079.cfilter(c)
 	return c:IsSetCard(0xac1)
 end
+function c95000079.condition(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsExistingMatchingCard(c95000079.cfilter,tp,LOCATION_SZONE+LOCATION_HAND,0,1,nil)
+	and Duel.GetFlagEffect(e:GetHandlerPlayer(),95000079)==0
+	and not e:GetHandler():IsStatus(STATUS_CHAINING)
+end
+function c95000079.Fcond(e)
+	local f1=Duel.GetFieldCard(0,LOCATION_SZONE,5)
+	local f2=Duel.GetFieldCard(1,LOCATION_SZONE,5)
+	return (f1:GetFlagEffect(195000079)==0) and (f2:GetFlagEffect(195000079)==0)
+end
+
 tableAction = {
 95000044,
 95000045,
-95000046
-} 
-tableAction_size=3
-
-function c95000079.filterStat(e,c)
-	return c:IsAttribute(ATTRIBUTE_EARTH)
+95000046,
+95000143
+}  
+function c95000079.repop(e)
+	local c=e:GetHandler()
+		if c:GetFlagEffect(900000007)==0 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_CHAIN_END)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetLabelObject(c)
+		e1:SetOperation(c95000079.returnop)
+		Duel.RegisterEffect(e1,0)
+		c:RegisterFlagEffect(900000007,0,0,1)
+	end
+	Duel.SendtoDeck(c,nil,-2,REASON_RULE)
 end
+function c95000079.returnop(e)
+	local c=e:GetLabelObject()
+	local tp=c:GetControler()
+	local fc=Duel.GetFieldCard(tp,LOCATION_SZONE,5)
+	if not fc then
+		Duel.MoveToField(c,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
+	end
+	if fc and fc:GetFlagEffect(195000079)==0 then
+	--action card get
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(95000079,0))
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCondition(c95000079.condition)
+	e1:SetTarget(c95000079.Acttarget)
+	e1:SetOperation(c95000079.operation)
+	e1:SetReset(RESET_EVENT+0x1fe0000)
+	fc:RegisterEffect(e1)
+	fc:RegisterFlagEffect(195000079,RESET_EVENT+0x1fe0000,0,1)
+	end
+end
+
