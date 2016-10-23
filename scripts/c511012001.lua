a2aa7226431fb7f112e05ca7ecca0e9be97c5523
Michael Lawrence C. Dee <michael_lawrence_dee@yahoo.com>
2016-10-23 16:35:56 +0800

    Odd-Eyes Pendulum Dragon fix

--- scripts/c511012001.lua
+++ scripts/c511012001.lua
@@ -1,14 +1,14 @@
---Odd-Eyes Pendulum Dragon (Anime)
+--オッドアイズ・ペンデュラム・ドラゴン
+--fixed by MLD
 function c511012001.initial_effect(c)
 	--pendulum summon
 	aux.EnablePendulumAttribute(c)
 	--reduce
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(511012001,0))
+	e2:SetDescription(aux.Stringid(16178681,0))
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_PRE_BATTLE_DAMAGE)
 	e2:SetRange(LOCATION_PZONE)
-	e2:SetCountLimit(1,511012001)
 	e2:SetCondition(c511012001.rdcon)
 	e2:SetOperation(c511012001.rdop)
 	c:RegisterEffect(e2)
@@ -21,17 +21,20 @@ function c511012001.initial_effect(c)
 	c:RegisterEffect(e4)
 end
 function c511012001.rdcon(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():GetFlagEffect(511012001)>0 then return false end
 	local tc=Duel.GetAttacker()
 	if tc:IsControler(1-tp) then tc=Duel.GetAttackTarget() end
 	return ep==tp and tc and tc:IsType(TYPE_PENDULUM)
 end
 function c511012001.rdop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.SelectYesNo(tp,aux.Stringid(511012001,0)) then
+	if Duel.SelectYesNo(tp,aux.Stringid(16178681,2)) then
+		e:GetHandler():RegisterFlagEffect(511012001,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
 		Duel.ChangeBattleDamage(tp,0)
 	end
 end
 function c511012001.damcon(e,tp,eg,ep,ev,re,r,rp)
-	return ep~=tp and e:GetHandler():GetBattleTarget()~=nil and e:GetHandler():GetBattleTarget():IsLevelAbove(5)
+	local bc=e:GetHandler():GetBattleTarget()
+	return ep~=tp and bc and bc:IsLevelAbove(5) and bc:IsControler(1-tp)
 end
 function c511012001.damop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.ChangeBattleDamage(ep,ev*2)
