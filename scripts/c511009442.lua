d246fe9dfc538d541d08f0fb3990e8caef094ed2
TheOnePharaoh <TheOnePharaoh@users.noreply.github.com>
2016-12-31 03:04:48 -0600

    Fixed Supreme King Servant Dragon Darkvrm (Anime)
    Fixed Effects

--- scripts/c511009442.lua
+++ scripts/c511009442.lua
@@ -2,23 +2,22 @@
 function c511009442.initial_effect(c)
 	--pendulum summon
 	aux.EnablePendulumAttribute(c)
-	--negate attack
+	--negate attack Pendulum Zone
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(80538728,0))
-	e1:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_BE_BATTLE_TARGET)
-	e1:SetRange(LOCATION_MZONE)
+	e1:SetRange(LOCATION_PZONE)
 	e1:SetCountLimit(1)
 	e1:SetCondition(c511009442.condition)
 	e1:SetOperation(c511009442.operation)
 	c:RegisterEffect(e1)
-	--negate attack 2
+	--negate attack Monster Zone
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(27143874,0))
-	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_BE_BATTLE_TARGET)
+	e2:SetRange(LOCATION_MZONE)	
 	e2:SetCondition(c511009442.con)
 	e2:SetOperation(c511009442.operation)
 	c:RegisterEffect(e2)
