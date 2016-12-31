97aab7a3a3595d12d78dfeb0e8d62e75e34d73ac
TheOnePharaoh <TheOnePharaoh@users.noreply.github.com>
2016-12-31 02:14:20 -0600

    Fixed Junk Warrior (Anime)

--- scripts/c511009416.lua
+++ scripts/c511009416.lua
@@ -10,7 +10,7 @@ function c511009416.initial_effect(c)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetCondition(c511009416.con)
-	e1:SetValue(c511009416.operation)
+	e1:SetValue(c511009416.value)
 	c:RegisterEffect(e1)
 end
 c511009416.material_setcode=0x17
@@ -23,7 +23,7 @@ end
 function c511009416.filter(c)
 	return c:IsFaceup() and c:IsLevelBelow(2)
 end
-function c511009416.operation(e,c)
+function c511009416.value(e,c)
 	local wup=0
 	local wg=Duel.GetMatchingGroup(c511009416.filter,c:GetControler(),LOCATION_MZONE,0,c)
 	local wbc=wg:GetFirst()
@@ -32,5 +32,4 @@ function c511009416.operation(e,c)
 		wbc=wg:GetNext()
 	end
 	return wup
-endnd
 end
