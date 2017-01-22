f4f4613fb1ee77a3a460e29907c43f86681545e3
GameMaster719 <cobaltss87@yahoo.com>
2017-01-21 19:15:43 -0700

    Fixes see below..
    1. Dark-eyes illusionist- face down issue - was wrong flag
    2. + Attribute Bomb (Anime)
    3. FIx for Atttribute Mastery - check for the equipped monster 
    cdb readjusted. / scripts reuploaded

--- scripts/c511002416.lua
+++ scripts/c511002416.lua
@@ -1,12 +1,11 @@
 --Dark eye illusionist
 --scripted by GameMaster(GM)
 function c511002416.initial_effect(c)
-      --negate attack
+      --negate attack and monster cannot declare attack
     local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(511000867,0))
     e2:SetType( EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
     e2:SetRange(LOCATION_MZONE)
-    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_SINGLE_RANGE)
     e2:SetCode(EVENT_BE_BATTLE_TARGET)
     e2:SetLabel(0)
     e2:SetCondition(c511002416.condition)
@@ -14,18 +13,19 @@ function c511002416.initial_effect(c)
     c:RegisterEffect(e2)
 end
 
-function c511002416.filter(c)
-    return c:IsFaceup() and c:IsCode(511002416)
-end
 function c511002416.condition(e,tp,eg,ep,ev,re,r,rp)
-    local ct=Duel.GetMatchingGroupCount(c511002416.filter,tp,LOCATION_MZONE,0,nil)
-    return not e:GetHandler():IsStatus(STATUS_CHAINING) and tp~=Duel.GetTurnPlayer()
+ local c=e:GetHandler()
+local bc=c:GetBattleTarget(38247752)
+ if  bc:IsFaceup() and bc:IsRelateToBattle() and Duel.GetAttackTarget()==c and not bc:IsStatus(STATUS_CHAINING) then return true
+else  return false end
 end
+
 function c511002416.activate(e,tp,eg,ep,ev,re,r,rp)
-    Duel.NegateAttack()
-    local c=e:GetHandler()
-    local bc=c:GetBattleTarget()
-    if bc and bc==Duel.GetAttacker() then
+     local c=e:GetHandler()
+     local bc=c:GetBattleTarget()
+ if not bc:IsRelateToBattle() then end	 
+	Duel.NegateAttack()
+ if bc and bc==Duel.GetAttacker() then
         Duel.Hint(HINT_CARD,0,511002416)
         local e1=Effect.CreateEffect(c)
         e1:SetType(EFFECT_TYPE_SINGLE)
